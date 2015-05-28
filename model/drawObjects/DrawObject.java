ab3c3ae6941b8317cec9ae70bc427fe4a63a063b
jancoow <jancokock@gmail.com>
2015-05-28 09:49:40 +0200

    Fixed double method name

--- model/drawObjects/DrawObject.java
+++ model/drawObjects/DrawObject.java
@@ -21,9 +21,6 @@ public abstract class DrawObject {
 		this.index = index;
 	}
 	
-	public void setIndex(int index){
-		this.index = index;
-	}
 	
 	public abstract void draw(Graphics2D g2);
 	public abstract void update();
