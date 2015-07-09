cb57ce7fe60a14c6d4bb19408d43b9c79be95d75
Kenneth van Ewijk <kennyboy@live.nl>
2015-07-09 20:13:17 +0200

    Fix for ledstrip

--- model/GameModel.java
+++ model/GameModel.java
@@ -41,7 +41,7 @@ public class GameModel {
 				
 				Color c = colors[(int) (Math.random() * (colors.length - 1) + 1)];
 				
-				for (int i = 6; i < 36; i++) { //TODO
+				for (int i = 6; i < 46; i++) { 
 					ntw.setLed(i, c.getRed(), c.getGreen(), c.getBlue());
 				}
 				
@@ -56,10 +56,10 @@ public class GameModel {
 				while(c1==c2)	
 					c2 = colors[(int) (Math.random() * (colors.length - 1) + 1)];
 				
-				for (int i = 6; i < 21; i++) { //TODO
+				for (int i = 6; i < 26; i++) {
 					ntw.setLed(i, c1.getRed(), c1.getGreen(), c1.getBlue());
 				}
-				for (int i = 22; i < 36; i++) { //TODO
+				for (int i = 27; i < 46; i++) {
 					ntw.setLed(i, c2.getRed(), c2.getGreen(), c2.getBlue());
 				}
 				
@@ -81,16 +81,16 @@ public class GameModel {
 				while(c5==c6)	
 					c6 = colors[(int) (Math.random() * (colors.length - 1) + 1)];
 				
-				for (int i = 6; i < 14; i++) { //TODO
+				for (int i = 6; i < 16; i++) {
 					ntw.setLed(i, c3.getRed(), c3.getGreen(), c3.getBlue());
 				}
-				for (int i = 15; i < 21; i++) { //TODO
+				for (int i = 17; i < 26; i++) {
 					ntw.setLed(i, c4.getRed(), c4.getGreen(), c4.getBlue());
 				}
-				for (int i = 22; i < 28; i++) { //TODO
+				for (int i = 27; i < 36; i++) {
 					ntw.setLed(i, c5.getRed(), c5.getGreen(), c5.getBlue());
 				}
-				for (int i = 28; i <36; i++) { //TODO
+				for (int i = 37; i <46; i++) {
 					ntw.setLed(i, c6.getRed(), c6.getGreen(), c6.getBlue());
 				}
 				
@@ -102,7 +102,7 @@ public class GameModel {
 				
 				Color c7;
 				
-				for (int i = 6; i < 36; i++) { //TODO
+				for (int i = 6; i < 46; i++) {
 					c7 = colors[(int) (Math.random() * (colors.length - 1) + 1)];
 					ntw.setLed(i, c7.getRed(), c7.getGreen(), c7.getBlue());
 				}
